cd3d1900a4c9456290d1e7bdd72d763015326724
John Caesar <john.v.caesar@gmail.com>
2021-06-30 16:06:20 +0300

    Exercise 1.7, unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,17 +3,39 @@ import './App.css';
 
 const Heading = ({ heading }) => <h1>{heading}</h1>
 const Button = ({ handleClick, name }) => <button onClick={handleClick}>{name}</button>
-const Display = ({ name, counter }) => <p>{name} {counter}</p>
+const Display = ({ name, counter, symbol }) => <p>{name} {counter} {symbol}</p>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  // allclicks are stored here
+  const [all, setAll] = useState(0)
 
-  const addGood = () => setGood(good + 1)
-  const addNeutral = () => setNeutral(neutral + 1)
-  const addBad = () => setBad(bad + 1)
+  const addGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const addBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  const average = () => {
+    if (all)
+      return (good-bad)/all
+    return 0
+  }
+  const positive = () => {
+    if (good)
+      return (good/all)*100
+    return 0
+  }
 
   return (
     <div>
@@ -28,6 +50,9 @@ const App = () => {
       <Display name='good  ' counter={good} />
       <Display name='neutral  ' counter={neutral} />
       <Display name='bad  ' counter={bad} />
+      <Display name='all  ' counter={all} />
+      <Display name='average  ' counter={average()} />
+      <Display name='positive  ' counter={positive()} symbol='%' />
     </div>
   )
 }
