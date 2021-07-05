08f2a88a3aa2ab1a885afb53ce1124829e202c3f
John Caesar <john.v.caesar@gmail.com>
2021-07-05 20:34:10 +0300

    Exercise 2.11: The Phonebook step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => <div>{person.name} {person.number}</div>
 
@@ -36,21 +37,21 @@ const Persons = ({ persons, filterWord }) => {
   )
 }
 
-
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterWord, setFilterWord ] = useState('')
 
+  const hook = () => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }
+
+  useEffect(hook, [])
+
   const nameExists = () => {
     const found = persons.find(name => name.name === newName)
-    //console.log('found: ', found)
     if (found)
       return true
     return false
@@ -58,13 +59,11 @@ const App = () => {
 
   const addNewName = (event) => {
     event.preventDefault()
-    //console.log('addnewname: ', event.target)
     const personObject = {
       name: newName,
       number: newNumber
     }
     if (nameExists()) {
-      //console.log(`${newName} exists in the array`)
       alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(personObject))
