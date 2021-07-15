8dbd41327918fb64241262487875962d1f236017
John Caesar <john.v.caesar@gmail.com>
2021-07-15 12:24:09 +0300

    Exercise 3.4: Phonebook backend, step 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 
 const app = express()
@@ -48,6 +49,12 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
